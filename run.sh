6a8ad775e29e352871a84d0464b99f1fdf406a0a
naoki-p <naokiwatanabe@live.com>
2022-11-18 20:47:44 +0900

    Change for WSLg

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --ipc=host --user pwuser --security-opt seccomp=seccomp_profile.json --volume $(pwd)/app:/home/pwuser/app pw:1.0 bash
+docker run -it --rm --ipc=host --user pwuser --security-opt seccomp=seccomp_profile.json --volume $(pwd)/app:/home/pwuser/app --env DISPLAY=$DISPLAY --volume /tmp/.X11-unix:/tmp/.X11-unix pw:1.0 bash
